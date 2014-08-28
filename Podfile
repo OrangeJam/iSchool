e28f2ffe7fd1c34cab43ff43bb56750739314223
Björn Orri Sæmundsson <bjornorri@gmail.com>
2014-08-28 14:44:04 +0000

    Added hpple parsing library

--- Podfile
+++ Podfile
@@ -1,11 +1,4 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, "8.0"
+platform :ios, "8.0"
 
-target "iSchool" do
-
-end
-
-target "iSchoolTests" do
-
-end
-
+pod 'hpple', '~> 0.2.0'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// hpple
+#define COCOAPODS_POD_AVAILABLE_hpple
+#define COCOAPODS_VERSION_MAJOR_hpple 0
+#define COCOAPODS_VERSION_MINOR_hpple 2
+#define COCOAPODS_VERSION_PATCH_hpple 0
+
