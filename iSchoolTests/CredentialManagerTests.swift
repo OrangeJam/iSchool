583e104e75a234ff4c86f68fe580d71ce487a343
Björn Orri Sæmundsson <bjornorri@gmail.com>
2014-09-01 08:41:44 +0000

    Unit tests for CredentialManager class ready

--- iSchoolTests/CredentialManagerTests.swift
+++ iSchoolTests/CredentialManagerTests.swift
@@ -26,17 +26,42 @@ class CredentialManagerTests: XCTestCase {
     func testStoreCredentials() {
         clearStoredCredentials()
         CredentialManager.sharedInstance.storeCredentials(user, pass)
-        XCTAssertEqual(user, userDefaults.stringForKey("username")!, "Username not stored correctly")
-        XCTAssertEqual(pass, userDefaults.stringForKey("password")!, "Password not stored correctly")
+        XCTAssertNotNil(userDefaults.stringForKey("username"), "Stored username is nil")
+        XCTAssertNotNil(userDefaults.stringForKey("password"), "Stored password is nil")
+        if(userDefaults.stringForKey("username") != nil) {
+            XCTAssertEqual(user, userDefaults.stringForKey("username")!, "Username not stored correctly")
+        }
+        
+        if(userDefaults.stringForKey("password") != nil) {
+            XCTAssertEqual(pass, userDefaults.stringForKey("password")!, "Password not stored correctly")
+        }
         clearStoredCredentials()
     }
     
     func testGetCredentials() {
+        clearStoredCredentials()
+        CredentialManager.sharedInstance.storeCredentials(user, pass)
+        let (username, password) = CredentialManager.sharedInstance.getCredentials()
+        XCTAssertNotNil(username, "Retreived username is nil")
+        XCTAssertNotNil(password, "Retreived password is nil")
+        if(username != nil) {
+            XCTAssertEqual(user, username!, "Username not retreived correctly")
+        }
         
+        if(password != nil) {
+            XCTAssertEqual(user, password!, "Password not retreived correctly")
+        }
+        clearStoredCredentials()
     }
     
     func testClearCredentials() {
-        
+        userDefaults.setObject(user, forKey: "username")
+        userDefaults.setObject(pass, forKey: "password")
+        userDefaults.synchronize()
+        CredentialManager.sharedInstance.clearCredentials()
+        XCTAssertNil(userDefaults.stringForKey("username"), "Username is not nil after clearing")
+        XCTAssertNil(userDefaults.stringForKey("password"), "Password is not nil after clearing")
+        clearStoredCredentials()
     }
     
     private func clearStoredCredentials() {
