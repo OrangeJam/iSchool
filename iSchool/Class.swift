0e57f9672b0fe42adee868072eeccafe728e2f14
bjornorri <bjornorri@gmail.com>
2014-09-04 14:49:55 +0000

    Created class model

---
+++ iSchool/Class.swift
@@ -0,0 +1,16 @@
+//
+//  Class.swift
+//  iSchool
+//
+//  Created by Björn Orri Sæmundsson on 04/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+struct Class {
+
+    var course = ""
+    var type = ""
+    var location = ""
+    var startDate = NSDate()
+    var endDate = NSDate()
+}
