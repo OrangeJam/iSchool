f9bb2c7a73413dcfe4198f3b7da5063a8d4db9f4
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-05 12:07:14 +0000

    changed assignmentsTableViewController to subclass UiTableViewController and added segue to IB

--- iSchool/AssignmentsTableViewController.swift
+++ iSchool/AssignmentsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AssignmentsTableViewController: UIViewController {
+class AssignmentsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
