c1329da713457b4c0e6bcccb84ce2f04431d364f
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-06 15:14:11 +0000

    refactored assignments struct to separate file

--- iSchool/Parser.swift
+++ iSchool/Parser.swift
@@ -8,31 +8,6 @@
 
 import UIKit
 
-struct Assignment {
-    var dueDate             = NSDate()
-    var handedIn            = false
-    var courseName          = ""
-    var courseIdentifier    = ""
-    var URL                 = ""
-    var name                = ""
-    
-    // I wish I had a better way to do this
-    init(attrs: [String]){
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        if let parsedDate = dateFormatter.dateFromString(attrs[0]) {
-            dueDate = parsedDate
-        }
-        if attrs[1] != "Óskilað" {
-            handedIn = true
-        }
-        courseName = attrs[2]
-        courseIdentifier = attrs[3]
-        URL = attrs[4]
-        name = attrs[5]
-    }
-}
-
 class Parser {
     
     class func parseAssignments(data: NSData) -> [Assignment]{
