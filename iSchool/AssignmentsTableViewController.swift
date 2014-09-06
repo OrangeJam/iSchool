e2f809b30a8159788316dc0f271e87ecf50af43b
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-06 14:15:02 +0000

    Working on implementing tableView.

--- iSchool/AssignmentsTableViewController.swift
+++ iSchool/AssignmentsTableViewController.swift
@@ -8,9 +8,51 @@
 
 import UIKit
 
-class AssignmentsTableViewController: UITableViewController {
+class AssignmentsTableViewCell : UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
+
+class AssignmentsTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableData: [Assignment] = []
     
     override func viewDidLoad() {
+        loadData()
         super.viewDidLoad()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AssignmentsTableViewCell") as AssignmentsTableViewCell
+        if (tableData.count > indexPath.row) {
+            let assignment = tableData[indexPath.row]
+            cell.nameLabel.text = "Sveppir"
+        }
+        return cell
+    }
+    
+    func loadData() {
+        let credentialManager = CredentialManager.sharedInstance
+        if let (username, password) = credentialManager.getCredentials() {
+            let networkClient = NetworkClient(username: username, password: password)
+            networkClient.fetchPage(Page.Assignments,
+                successHandler: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+                    let responseData = NSData(data: response as NSData)
+                    self.tableData = Parser.parseAssignments(responseData)
+                    self.tableView.reloadData()
+                },
+                errorHandler: { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                    NSLog("Error: \(error.description)")
+                }
+            )
+        }
     }
 }
