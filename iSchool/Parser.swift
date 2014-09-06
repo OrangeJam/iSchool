db1505c704d03dae9e1ca5451c0774e1fde7226e
bjornorri <bjornorri@gmail.com>
2014-09-06 20:13:23 +0000

    Parsing of timetable well on its way

--- iSchool/Parser.swift
+++ iSchool/Parser.swift
@@ -43,7 +43,47 @@ class Parser {
     }
     
     class func parseClasses(data: NSData) -> [Class] {
+        let parser = TFHpple.hppleWithHTMLData(data)
         var classes: [Class] = []
+        let xpathQuery = "//div[@class='ruContentPage']/center[1]/table/tbody/tr"
+        var nodes = parser.searchWithXPathQuery(xpathQuery) as [TFHppleElement]
+        if nodes.count > 0 {
+            // Ignore irrelevant nodes (first two and the last)
+            nodes.removeLast()
+            nodes.removeRange(Range(start:0, end:2))
+            
+            // Extract and remove the row that holds the dates
+            let dateRow = nodes[0]
+            nodes.removeAtIndex(0)
+            
+            // Get the dates in an array
+            var dates: [String] = []
+            var dateColumns = dateRow.childrenWithTagName("th")
+            // Remove first irrelevant  column
+            dateColumns.removeAtIndex(0)
+            for column in dateColumns as [TFHppleElement] {
+                if let text = column.text() {
+                    dates.append(text)
+                }
+            }
+            for row in nodes as [TFHppleElement] {
+                // Split the row into columns
+                var columns = row.childrenWithTagName("td")
+                // Extract the column with the start and end time and then remove it
+                let timeColumn: TFHppleElement = columns[0] as TFHppleElement
+                columns.removeAtIndex(0)
+                for column in columns as [TFHppleElement] {
+                    if column.childrenWithTagName("span").count > 0 {
+                        for classSpan in column.childrenWithTagName("span") as [TFHppleElement] {
+                            // Need this stupid double cast...
+                            if let infoString = classSpan.attributes["title"] as? NSString as? String{
+                                NSLog(infoString)
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return classes
     }
 }
