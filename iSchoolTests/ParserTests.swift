39a9a896dbf121099a47c94cb98cff48cc6b2b3e
bjornorri <bjornorri@gmail.com>
2014-09-06 21:59:36 +0000

    Fixed wrong unit tests for timetable parsing

--- iSchoolTests/ParserTests.swift
+++ iSchoolTests/ParserTests.swift
@@ -37,14 +37,13 @@ class ParserTests: XCTestCase {
         // Test if all classes were parsed
         XCTAssertEqual(classes.count, 22, "There should be 22 classes")
         
-        // Create dates
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let startDate = dateFormatter.dateFromString("02.09.2014 13:10")
-        let endDate = dateFormatter.dateFromString("02.09.2014 13:55")
         
         // Test the class at index 6
         if(classes.count > 6) {
+            let startDate = dateFormatter.dateFromString("02.09.2014 13:10")
+            let endDate = dateFormatter.dateFromString("02.09.2014 13:55")
             XCTAssertEqual(classes[6].course, "Tölvusamskipti", "The course name of the class at index 6 should be Tölvusamskipti")
             XCTAssertEqual(classes[6].type, ClassType.Lecture, "The course at index 6 should be a lecture")
             XCTAssertEqual(classes[6].location, "V201", "The location of the course at index 6 should be V201")
@@ -54,12 +53,14 @@ class ParserTests: XCTestCase {
             XCTFail("Could not test class at index 6")
         }
         
-        // Test the class at index 8 (note same start and end dates)
+        // Test the class at index 8
         if(classes.count > 8) {
+            let startDate = dateFormatter.dateFromString("03.09.2014 13:10")
+            let endDate = dateFormatter.dateFromString("03.09.2014 13:55")
             XCTAssertEqual(classes[8].course, "Stærðfræði I", "The course name of the class at index 8 should be Stærðfræði I")
             XCTAssertEqual(classes[8].type, ClassType.Discussion, "The class at index 8 should be a discussion class")
             XCTAssertEqual(classes[8].location, "V105", "The location of the course at index 8 should be V105")
-            XCTAssertEqual(classes[8].startDate, startDate!, "The class at index 8 should start at 02.09.2014 13:10")
+            XCTAssertEqual(classes[8].startDate, startDate!, "The class at index 8 should start at 03.09.2014 13:10")
             XCTAssertEqual(classes[8].endDate, endDate!, "The class at index 8 should end at 02.09.2014 13:55")
         } else {
             XCTFail("Could not test class at index 8")
@@ -74,14 +75,13 @@ class ParserTests: XCTestCase {
         // Test if all classes (exams) were parsed
         XCTAssertEqual(classes.count, 8, "There should be 8 classes (exams)")
         
-        // Create dates
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
-        let startDate = dateFormatter.dateFromString("08.04.2014 15:45")
-        let endDate = dateFormatter.dateFromString("08.04.2014 16:30")
         
         // Test the class at index 2
         if(classes.count > 2) {
+            let startDate = dateFormatter.dateFromString("08.04.2014 15:45")
+            let endDate = dateFormatter.dateFromString("08.04.2014 16:30")
             XCTAssertEqual(classes[2].course, "Forritunarmál", "The course name of the class (exam) at index 2 should be Forritunarmál")
             XCTAssertEqual(classes[2].type, ClassType.Exam, "The class (exam) at index 2 should have the type Exam")
             XCTAssertEqual(classes[2].location, "M201", "The class (exam) at index 2 should have the location M201")
@@ -93,6 +93,8 @@ class ParserTests: XCTestCase {
         
         // Test the class at index 6
         if(classes.count > 6) {
+            let startDate = dateFormatter.dateFromString("11.04.2014 15:45")
+            let endDate = dateFormatter.dateFromString("11.04.2014 16:30")
             XCTAssertEqual(classes[6].course, "Hugbúnaðarfræði II", "The course name of the class (exam) at index 6 should be Hugbúnaðarfræði II")
             XCTAssertEqual(classes[6].type, ClassType.Exam, "The class (exam) at index 6 should have the type Exam")
             XCTAssertEqual(classes[6].location, "M101", "The class (exam) at index 6 should have the location M101")
