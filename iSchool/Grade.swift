dd4a30b7236c1ab5aa630d11825f2dc63eb8827b
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-08 14:17:34 +0000

    Added grade struct

---
+++ iSchool/Grade.swift
@@ -0,0 +1,17 @@
+//
+//  Grade.swift
+//  iSchool
+//
+//  Created by Kári Helgason on 08/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import Foundation
+
+struct Grade {
+    var name: String
+    var grade: Float
+    var order: String
+    var feedback: String
+    var course: String
+}
