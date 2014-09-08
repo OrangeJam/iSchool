da2d730b1d9508530f032ad867dffdd3c5a28abd
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-08 11:06:32 +0000

    Added empty data store implementation

---
+++ iSchool/DataStore.swift
@@ -0,0 +1,13 @@
+//
+//  DataStore.swift
+//  iSchool
+//
+//  Created by Kári Helgason on 08/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+
+class DataStore {
+    
+}
---
+++ iSchoolTests/DataStoreTest.swift
@@ -0,0 +1,24 @@
+//
+//  DataStoreTest.swift
+//  iSchool
+//
+//  Created by Kári Helgason on 08/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DataStoreTests: XCTestCase {
+    
+    var networkClient = NetworkClient(username: "test", password: "test")
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
