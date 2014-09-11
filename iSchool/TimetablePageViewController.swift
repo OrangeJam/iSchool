62ba4388429cfb6a16cce18d0bdc1e1216fb5c76
bjornorri <bjornorri@gmail.com>
2014-09-11 16:08:51 +0000

    Created ViewControllers for timetable

---
+++ iSchool/TimetablePageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimetablePageViewController.swift
+//  iSchool
+//
+//  Created by Björn Orri Sæmundsson on 11/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+
+class TimetablePageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
