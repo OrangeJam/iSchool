29a345a0faa38d95a2c95a1b80c4bdf104475460
bjornorri <bjornorri@gmail.com>
2014-09-11 15:23:44 +0000

    Created ClassCell class

---
+++ iSchool/ClassCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassCell.swift
+//  iSchool
+//
+//  Created by Björn Orri Sæmundsson on 11/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+
+class ClassCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
