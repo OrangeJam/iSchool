aea0c144c3876c07bde7e8550d24614f9f0d3ff1
bjornorri <bjornorri@gmail.com>
2014-09-11 13:16:38 +0000

    Added test for getClassesForDay

--- iSchoolTests/DataStoreTest.swift
+++ iSchoolTests/DataStoreTest.swift
@@ -11,6 +11,15 @@ import XCTest
 
 class DataStoreTests: XCTestCase {
     
+    class DataStoreMock: DataStore {
+        // Fetch classes from test page instead of the real page
+        override func fetchClasses() {
+            let testDataPath = NSBundle(forClass: ParserTests.self).pathForResource("timetable_normal", ofType: "html")!
+            let data = NSData(contentsOfFile: testDataPath)
+            classes = Parser.parseClasses(data)
+        }
+    }
+    
     override func setUp() {
         super.setUp()
     }
@@ -34,5 +43,23 @@ class DataStoreTests: XCTestCase {
         waitForExpectationsWithTimeout(10, handler: { _ in
             
         })
+    }
+    
+    func testGetClassesForDay() {
+        let STOR = "Stöðuvélar og reiknanleiki"
+        let ANDR = "App Development - Android"
+        let dataStore = DataStoreMock()
+        dataStore.fetchClasses()
+        let classes = dataStore.getClassesForDay(WeekDay.Monday)
+        XCTAssertEqual(classes.count, 5, "Classes for day not fetched correctly")
+        if classes.count == 5 {
+            XCTAssertEqual(classes[0].course, STOR, "First class on Monday should be STOR")
+            XCTAssertEqual(classes[1].course, STOR, "Second class on Monday should be STOR")
+            XCTAssertEqual(classes[2].course, ANDR, "Third class on Monday should be ANDR")
+            XCTAssertEqual(classes[3].course, ANDR, "Fourth class on Monday should be ANDR")
+            XCTAssertEqual(classes[4].course, ANDR, "Fifth class on Monday should be ANDR")
+        } else {
+            XCTFail("Could not check individual classes in testGetClassesForDay")
+        }
     }
 }
