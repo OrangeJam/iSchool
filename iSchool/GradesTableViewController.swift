c4a7cc23cd47c4a8691ddcfbc8b3ee1a3ae1e94c
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-16 12:55:26 +0000

    Added grades cell subclass

--- iSchool/GradesTableViewController.swift
+++ iSchool/GradesTableViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+class GradesTableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
+
 class GradesTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataStore.sharedInstance.getAssignments().count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let grades = DataStore.sharedInstance.getGrades()
+        let cell = tableView.dequeueReusableCellWithIdentifier("GradesTableViewCell") as GradesTableViewCell
+        cell.nameLabel.text = grades[indexPath.row].name
+        return cell
     }
 
 }
