3daadbb050957d6ac9c779f42db46d617df9db96
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-16 12:49:48 +0000

    Added empty class and storyboard for grades table view

---
+++ iSchool/GradesTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GradesTableViewController.swift
+//  iSchool
+//
+//  Created by Kári Helgason on 16/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+
+class GradesTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
