9e8a2ac236f0706ba783f703b655a1a888e4d0f0
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-17 15:35:58 +0000

    Added empty implementation for the tab bar controller

---
+++ iSchool/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  iSchool
+//
+//  Created by Kári Helgason on 17/09/14.
+//  Copyright (c) 2014 OrangeJam. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController : UITabBarController {
+    
+    
+    
+}
