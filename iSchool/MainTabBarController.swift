455d4bfc1ed0e7289c5d594bff7b6fdacc31d48b
Kári Tristan Helgason <kthelgason@gmail.com>
2014-09-18 13:45:16 +0000

    Handling wrong credentials

--- iSchool/MainTabBarController.swift
+++ iSchool/MainTabBarController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class MainTabBarController : UITabBarController {
     
+    override func viewDidLoad() {
+        NSNotificationCenter.defaultCenter().addObserverForName(
+            Notification.networkError.toRaw(),
+            object: nil,
+            queue: NSOperationQueue.mainQueue(),
+            usingBlock: { _ in
+                self.performSegueWithIdentifier("presentLoginView", sender: self)
+            }
+        )
+        super.viewDidLoad()
+    }
+    
     override func viewDidAppear(animated: Bool) {
         if let credentials = CredentialManager.sharedInstance.getCredentials() {
+            println("Creds: \(credentials)")
             DataStore.sharedInstance.fetchAssignments()
         } else {
             performSegueWithIdentifier("presentLoginView", sender: self)
         }
     }
-    
-    
 }
