d036f94029a9ad63bb1632ee09a57151c73759f6
Kári Tristan Helgason <kthelgason@gmail.com>
2014-11-03 09:40:07 +0000

    Implemented logout

--- iSchool/MoreMenuController.swift
+++ iSchool/MoreMenuController.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
-class MoreMenuController: UITableViewController {
+class MoreMenuController: UITableViewController, UITableViewDelegate {
     
+    override func viewDidLoad() {
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        switch(indexPath.row) {
+        case 2:
+            println("Logout")
+            logOutUser()
+        default:
+            println("HEHE flippari selectaði röð")
+        }
+    }
+    
+    func logOutUser() {
+        CredentialManager.sharedInstance.clearCredentials()
+        if let tabbar = self.tabBarController as? MainTabBarController {
+            tabbar.presentLoginView()
+        }
+    }
 }
